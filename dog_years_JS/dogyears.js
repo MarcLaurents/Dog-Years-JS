a80911b9ff2e97a60f6ced2116308408561a34e6
Marcelo Laurentino <90254883+MarcLaurents@users.noreply.github.com>
2022-02-02 05:17:55 -0300

    Add files via upload
    2.2.22 update

---
+++ dog_years_JS/dogyears.js
@@ -0,0 +1,24 @@
+// Calculate human age in dog years
+// My age
+const myAge = 26
+// Early years of dog
+let earlyYears = 2
+// Multiply earlyYears for 10.5
+earlyYears *= 10.5
+// Subtract 2 years from myAge
+laterYears = myAge - 2
+// Calculate number of dog years
+laterYears *= 4
+// Log earlyYears and laterYears
+console.log(`Early years: ${earlyYears} | Late years: ${laterYears}.`)
+// Calculate myAge in dog years
+myAgeInDogYears = earlyYears + laterYears
+// My name to lower case
+const myName = 'Marcelo Laurentino'.toLowerCase()
+// Log my name, my age, and my age in dog years
+console.log(
+  `My name is ${myName}. I am ${myAge} years old in human years wich is ${myAgeInDogYears} years old in dog years.`
+)
+// Final log
+// Early years: 21 | Late years: 96.
+// My name is marcelo laurentino. I am 26 years old in human years wich is 117 years old in dog years.
